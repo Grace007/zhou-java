dd1f4279df0c2a25199063b12b362e0d72b86e36
821132246@qq.com <15809713216run>
2017-09-18 21:30:55 +0800

    1

--- src/main/java/com/zhou/ApplicationRun.java
+++ src/main/java/com/zhou/ApplicationRun.java
@@ -1,8 +1,23 @@
 package com.zhou;
 
+import com.zhou.test.SpringContextUtilTest;
+import com.zhou.util.SpringContextUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author eli
  * @date 2017/9/18 15:40
  */
 public class ApplicationRun {
+    private static Logger logger= LoggerFactory.getLogger(ApplicationRun.class);
+
+    public static void main(String[] args) {
+        logger.info(">>>>>zhou-dome项目开始启动<<<<<<<");
+        new ClassPathXmlApplicationContext("classpath:spring/*.xml");
+        SpringContextUtilTest springContextUtilTest =(SpringContextUtilTest) SpringContextUtil.getBean("springContextUtilTest");
+        springContextUtilTest.show();
+        logger.info(">>>>>zhou-dome项目完成启动<<<<<<<");
+    }
 }
---
+++ src/main/java/com/zhou/test/SpringContextUtilTest.java
@@ -0,0 +1,11 @@
+package com.zhou.test;
+
+/**
+ * @author eli
+ * @date 2017/9/18 19:25
+ */
+public class SpringContextUtilTest {
+    public void show(){
+        System.out.println("SpringContextUtil正常");
+    }
+}
